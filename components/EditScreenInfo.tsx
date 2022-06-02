f665a642d7a1aab8c951077179d81fe01ed46f93
StefanBurscher <stefan.burscher@gmail.com>
2022-06-02 12:50:39 +0200

    ..

--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -1,17 +1,8 @@
 import React, { useState } from "react";
-import {
-  // Button,
-  FlatList,
-  Platform,
-  StyleSheet,
-  // TextInput,
-  TouchableOpacity,
-} from "react-native";
-import * as Linking from "expo-linking";
+import { StyleSheet } from "react-native";
 
-import { Text, View } from "./Themed";
+import { View } from "./Themed";
 
-import axios from "axios";
 import AddProfile from "./organisms/AddProfile";
 import ListGroup from "./organisms/ListGroup";
 import ProfileList from "./organisms/ProfileList";
@@ -22,62 +13,37 @@ const lists = [
 ];
 
 export default function EditScreenInfo() {
-  const [inputValue, setInputValue] = useState("");
   const [allGroups, setAllGroups] = useState(lists);
   const [activeGroupIndex, setActiveGroupIndex] = useState(0);
 
-  const addToList = () => {
-    const newList = [...allGroups];
+  // const getPhoto = (a) => {
+  //   // validation for instagram usernames
+  //   var regex = new RegExp(/^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/);
+  //   var validation = regex.test(a);
 
-    const splittedValue = inputValue.split("instagram.com/");
-    const newValue = splittedValue.length > 1 ? splittedValue[1] : inputValue;
+  //   if (validation) {
+  //     try {
+  //       const data = axios.get("https://www.instagram.com/" + a + "/?__a=1");
 
-    const handleValue = newValue.split("?")[0];
+  //       // getting the url
+  //       var photoURL = data["graphql"]["user"]["profile_pic_url_hd"];
+  //       console.log(
+  //         "🚀 ~ file: EditScreenInfo.tsx ~ line 46 ~ EditScreenInfo ~ photoURL",
+  //         photoURL
+  //       );
 
-    // getPhoto(handleValue);
-
-    newList[activeGroupIndex].items.push({ handle: handleValue });
-    setInputValue("");
-  };
-
-  const getPhoto = (a) => {
-    // validation for instagram usernames
-    var regex = new RegExp(/^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/);
-    var validation = regex.test(a);
-
-    if (validation) {
-      try {
-        const data = axios.get("https://www.instagram.com/" + a + "/?__a=1");
-
-        // getting the url
-        var photoURL = data["graphql"]["user"]["profile_pic_url_hd"];
-        console.log(
-          "🚀 ~ file: EditScreenInfo.tsx ~ line 46 ~ EditScreenInfo ~ photoURL",
-          photoURL
-        );
-
-        // update img element
-        // $("#photoReturn").attr("src", photoURL);
-        // })
-        // .fail(function () {
-        //   // code for 404 error
-        //   alert("Username was not found!");
-        // });
-      } catch (error) {}
-    } else {
-      alert("The username is invalid!");
-    }
-  };
-
-  const openProfile = (item) => {
-    const url = `https://instagram.com/${item.handle}`;
-
-    if (Platform.OS == "web") {
-      window.open(url, "_blank");
-    } else {
-      Linking.openURL(url);
-    }
-  };
+  //       // update img element
+  //       // $("#photoReturn").attr("src", photoURL);
+  //       // })
+  //       // .fail(function () {
+  //       //   // code for 404 error
+  //       //   alert("Username was not found!");
+  //       // });
+  //     } catch (error) {}
+  //   } else {
+  //     alert("The username is invalid!");
+  //   }
+  // };
 
   const addToGroup = (newItem) => {
     setAllGroups(
@@ -114,13 +80,5 @@ const styles = StyleSheet.create({
   getStartedContainer: {
     alignItems: "center",
     marginHorizontal: 50,
-  },
-
-  // profile item
-  item: {
-    backgroundColor: "#f9c2cc",
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
   },
 });
